ebe19662d58be050949beb348c990991a1178e30
Valeria-Miguel <2022371092@uteq.edu.mx>
2025-07-06 23:34:00 -0600

    Primer commit - proyecto menchaca

---
+++ src/app/pages/auth/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  imports: [
+    FormsModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule
+  ]
+})
+export class LoginComponent {}
---
+++ src/app/pages/consultorios/consultorios.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consultorios',
+  imports: [],
+  templateUrl: './consultorios.component.html',
+  styleUrl: './consultorios.component.css'
+})
+export class ConsultoriosComponent {
+
+}
